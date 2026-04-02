3b024e2e2b780a508658f620897e126325d58619
Naman Yadav <namany2006@gmail.com>
2026-04-02 23:02:18 +0530

    src index file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const PUBLISHABLE_KEY = "pk_test_aW5ub2NlbnQtdHVydGxlLTg4LmNsZXJrLmFjY291bnRzLmRldiQ" ||
+  process.env.REACT_APP_CLERK_PUBLISHABLE_KEY ||
+  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Clerk publishable key');
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <App />
-  </React.StrictMode>
+  </ClerkProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
